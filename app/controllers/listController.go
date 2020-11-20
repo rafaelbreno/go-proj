d09b1cd1c883883b8a4ee906ecbe29ad9fe9cf7c
Rafael Breno <rafael_breno@outlook.com>
2020-11-20 12:07:49 -0300

    Add update and delete controller methods

--- app/controllers/listController.go
+++ app/controllers/listController.go
@@ -41,3 +41,35 @@ func Show(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": list})
 }
+
+func Update(c *gin.Context) {
+	var list models.List
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&list).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found"})
+		return
+	}
+
+	var input models.ListInput
+
+	if err := c.ShouldBind(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	models.DB.Model(&list).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": list})
+}
+
+func Delete(c *gin.Context) {
+	var list models.List
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&list).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found"})
+		return
+	}
+
+	models.DB.Delete(&list)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
+}
