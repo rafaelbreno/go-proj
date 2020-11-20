f24592af4bd54a0444ec139f15ac080eafa2f72f
Rafael Breno <rafael_breno@outlook.com>
2020-11-20 11:54:30 -0300

    Add store and show methods into list controller

--- app/controllers/listController.go
+++ app/controllers/listController.go
@@ -12,3 +12,32 @@ func Index(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": lists})
 }
+
+func Store(c *gin.Context) {
+	var input models.ListInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	list := models.List{
+		UserId: input.UserId,
+		Title:  input.Title,
+		Status: input.Status,
+	}
+
+	models.DB.Create(&list)
+
+	c.JSON(http.StatusOK, gin.H{"data": list})
+}
+
+func Show(c *gin.Context) {
+	var list models.List
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&list).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": list})
+}
