e7b72b919888c17ab5e95fe0d135aaee27c04e0f
Rafael Breno <rafael_breno@outlook.com>
2020-11-25 23:37:18 -0300

    Implement menu to run tests, prune images, deploy containers, etc.

--- start.sh
+++ start.sh
@@ -19,6 +19,31 @@ if [ ! -f "$ENV" ]; then
     exit
 fi
 
-# Deploying/Up Containeres/Services
-echo "- Building and deploying containeres"
-docker-compose build && docker-compose up
+echo "Choose an option:"
+echo "1- Run Tests"
+echo "2- Run Prod"
+echo "3- Run Tests and Prod"
+echo "4- Purge All"
+
+read opt
+
+if [ $opt == "1" ]; then
+    # Running tests 
+    docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit
+    docker-compose down
+elif [ $opt == "2" ]; then
+    # Deploying/Up Containeres/Services
+    echo "- Building and deploying containeres"
+    docker-compose build && docker-compose up
+elif [ $opt == "3" ]; then
+    # Running tests 
+    docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit
+    docker-compose down
+    # Deploying/Up Containeres/Services
+    echo "- Building and deploying containeres"
+    docker-compose build && docker-compose up
+elif [ $opt == "4" ]; then
+    docker-compose down --remove-orphans --volumes
+    docker system prune -a --volumes
+fi
+
