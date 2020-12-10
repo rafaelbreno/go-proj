1a797f0673a92f17bd35fc5264f2eee473430f27
Rafael Breno <rafael_breno@outlook.com>
2020-12-10 20:05:37 -0300

    Add return to stop method from continuing execution

--- app/handler/userHandler.go
+++ app/handler/userHandler.go
@@ -31,6 +31,7 @@ func (uh *UserHandlers) FindById(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"user": user})
