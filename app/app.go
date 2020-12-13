b66a434887b3dcab5ee1cef0a0620d4701eb1280
Rafael Breno <rafael_breno@outlook.com>
2020-12-12 22:41:46 -0300

    Implement logger

--- app/app.go
+++ app/app.go
@@ -1,11 +1,15 @@
 package app
 
 import (
+	"go-proj/cmd/logger"
 	"go-proj/domain"
 	"go-proj/routes"
 )
 
 func Innit() {
+	// Setting App's logger
+	logger.Innit()
+
 	// Setting DB cmd
 	domain.Setting()
 
