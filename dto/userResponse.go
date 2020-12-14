a90c0241839f99d6210763bbbdc9f1d92eb4ba75
Rafael Breno <rafael_breno@outlook.com>
2020-12-14 20:33:58 -0300

    Remove user dto's password index

--- dto/userResponse.go
+++ dto/userResponse.go
@@ -1,10 +1,9 @@
 package dto
 
 type UserResponse struct {
-	ID       uint   `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Status   uint   `json:"status"`
+	ID     uint   `json:"id"`
+	Email  string `json:"email"`
+	Status uint   `json:"status"`
 
 	// Account can be a uuid string
 	Account string `json:"account"`
